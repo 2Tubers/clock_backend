d1f25d7700d9b15d7f24fafa4cbaecc0de2bae85
2Tubers <sankalps1111@gmail.com>
2024-08-03 12:56:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,92 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from "body-parser"; 
+import cors from 'cors';
+import env from 'dotenv';
+const app = express();
+
+app.use(cors());
+  // Function to search for a track by name
+  var songPreviewUrl="";
+  env.config();
+   
+  async function searchTrack(songName) {
+      try {
+          // Spotify API credentials
+          const clientId=process.env.client_Id;
+          const clientSecret=process.env.client_Secret;
+          
+          
+          // Request access token
+          const body={
+              grant_type:'client_credentials',
+              client_id:clientId,
+              client_secret:clientSecret  
+          };
+          const authResponse=await axios.post('https://accounts.spotify.com/api/token',
+          body,
+          {
+              headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded'
+              }
+          }
+          );
+          
+          const accessToken = authResponse.data.access_token;
+          // console.log('Access Token:', accessToken);
+  
+          // Search for track using the songName
+          const searchResponse = await axios.get('https://api.spotify.com/v1/search', {
+              params: {
+                  q: songName,
+                  type: 'track'
+              },
+              headers: {
+                  'Authorization': `Bearer ${accessToken}`
+              }
+          });
+  
+          //Get the song preview using the trackId
+          const trackId=searchResponse.data.tracks.items[0].id;
+  
+        const songPreview = await axios.get(`https://api.spotify.com/v1/tracks/${trackId}`, {
+          headers: {
+            'Authorization': `Bearer ${accessToken}`
+          }
+        });
+        
+       songPreviewUrl=songPreview.data.preview_url;
+     
+        console.log(songPreviewUrl);
+          return songPreviewUrl;
+      } catch (error) {
+          console.error('Error:', error.message);
+          return null;
+      }
+  }
+
+// searchTrack("tennu le");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/', async (req, res) => {
+  try {
+    const response = await searchTrack(req.body.name);
+    // console.log(response);
+    res.send(response);
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Example app listening on port 5000!');
+});
+
+
+
+
+
+  
